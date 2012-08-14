02830dacf72cc9509cc1443a5b54b7f82b992ea2
John OBrien <jkobrien@yahoo.com>
2012-08-14 14:23:39 +0100

    third commit

--- xbox-360-forensics-toolkit_tmphhp/xbox-360-forensics-toolkit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Compiling Help File
-"C:\Program Files (x86)\vahelp\help-hcw\hhc.exe" "C:\Data\PRACTI~1\Toolkit\BUFFAL~1\xbox-360-forensics-toolkit_tmphhp\xbox-360-forensics-toolkit.hhp"
-pause
